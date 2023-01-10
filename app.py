a633d70dd0411ca7aaae1bd64642bde2df3d9da5
IrinaShalaeva678 <00irinaschalaeva00@gmail.com>
2023-01-10 22:31:09 +0500

    Better organizing

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ def predict():
     return 'Hello World!'
 
 with gr.Blocks() as app:
-    image = gr.Image()
-    button = gr.Button()
-    text = gr.Label()
+    with gr.Row() as row:
+        with gr.Column() as col_1:
+            image = gr.Image()
+            button = gr.Button()
+        with gr.Column() as col_2:
+            text = gr.Label()
     button.click(fn=predict, outputs=text)
 
 if __name__ == '__main__':
