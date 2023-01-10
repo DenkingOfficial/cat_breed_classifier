7e8d08334235314816cc7c82e50ad198c7678345
rancelyndar <aeabr01@gmail.com>
2023-01-11 00:26:35 +0500

    Move list to other file

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 import gradio as gr
 import tensorflow as tf
 import numpy as np
+from cat_breeds_list import CAT_BREEDS
 
-CAT_BREEDS = ['Бенгальская', 'Бомбейская', 'Бритнаская короткошерстная',    \
-              'Бурмилла', 'Девон-рекс', 'Европейская короткошерстная',      \
-              'Экзотическая короткошерстная', 'Мейн-кун', 'Нибелунг',       \
-              'Персидская','Питерболд', 'Рэгдолл', 'Русская голубая',       \
-              'Саванна','Шотландская вислоухая','Сибирская','Сингапурская', \
-              'Сомалийская','Сфинкс','Черепаховая']
-
-model = tf.keras.models.load_model('./models/20_cat_classes_model.h5')
+MODEL = tf.keras.models.load_model('./models/20_cat_classes_model.h5')
 
 def predict(image):
     image = image.resize((128, 128))
     image = np.asarray(image)
     image = image.reshape(1, 128, 128, 3)
-    prediction = model.predict(image)[0]
+    prediction = MODEL.predict(image)[0]
     return {CAT_BREEDS[i]: float(prediction[i]) for i in range(20)}
 
 with gr.Blocks() as app:
