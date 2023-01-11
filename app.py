11e01fd3447c04013a3492b18f843a6b38e40991
IrinaShalaeva678 <00irinaschalaeva00@gmail.com>
2023-01-11 21:41:09 +0500

    Added styles and assigned element IDs

--- app.py
+++ app.py
@@ -14,14 +14,16 @@ def predict(image):
     breed_description = CAT_DESCRIPTIONS[predicted_breed]
     return {CAT_BREEDS[i]: float(prediction[i]) for i in range(20)}, breed_description
 
-with gr.Blocks() as app:
-    with gr.Row() as row:
-        with gr.Column(scale=2) as col_1:
-            image = gr.Image(label='Загрузите фотографию котика сюда', type='pil')
+with gr.Blocks(css='./static/style.css') as app:
+    with gr.Row(elem_id='main-row') as row:
+        with gr.Column(scale=2, elem_id='first-col') as col_1:
+            image = gr.Image(label='Загрузите фотографию котика сюда', type='pil', \
+                             elem_id='user-image')
             button = gr.Button(value='Определить породу')
-        with gr.Column(scale=1) as col_2:
-            text = gr.Label(num_top_classes=5, label='Результат определения породы')
-    breed_description = gr.Markdown()
+        with gr.Column(scale=1, elem_id='second-col') as col_2:
+            text = gr.Label(num_top_classes=5, label='Результат определения породы', \
+                            elem_id='predictions_text')
+    breed_description = gr.Markdown(elem_id='description')
     button.click(fn=predict, inputs=[image], outputs=[text, breed_description])
 
 if __name__ == '__main__':
