cdd2a769a09554777cfe4311712ab815f043cbac
DenkingOfficial <shershnev.andrei@gmail.com>
2023-01-11 23:16:44 +0500

    Even better formatting

--- app.py
+++ app.py
@@ -17,32 +17,40 @@ def predict(image):
     return {CAT_BREEDS[i]: float(prediction[i]) for i in range(20)}, breed_description
 
 with gr.Blocks(css='./static/style.css') as app:
-    
+
     gr.Markdown(
         """
         # Классификатор пород котов
         Разработано студентами Шершневым А.А, Ивановым С.С, Шалаевой И.Г. и Ильиным С.С.
         Группы: РИМ-120906, РИМ-120907
-        """, elem_id='md-text')
+        """,
+        elem_id='md-text'
+    )
     
     with gr.Row(elem_id='main-row') as row:
 
         with gr.Column(scale=2, elem_id='first-col') as col_1:
-            user_image = gr.Image(label='Загрузите фотографию котика сюда',        \
-                                  type='pil',                                      \
-                                  elem_id='user-image')
+            user_image = gr.Image(
+                label='Загрузите фотографию котика сюда',
+                type='pil',
+                elem_id='user-image'
+            )
             predict_button = gr.Button(value='Определить породу')
         
         with gr.Column(scale=1, elem_id='second-col') as col_2:
-            predicted_labels = gr.Label(num_top_classes=5,                         \
-                                        label='Результат определения породы',      \
-                                        elem_id='predictions-text')
+            predicted_labels = gr.Label(
+                num_top_classes=5,
+                label='Результат определения породы',
+                elem_id='predictions-text'
+            )
     
     breed_description = gr.Markdown(elem_id='breed-description')
-    
-    predict_button.click(fn=predict,                                               \
-                         inputs=[user_image],                                      \
-                         outputs=[predicted_labels, breed_description])
+
+    predict_button.click(
+        fn=predict,
+        inputs=[user_image],
+        outputs=[predicted_labels, breed_description]
+    )
 
 if __name__ == '__main__':
     app.launch(debug=True)
