b7ae7a2920affc00f90bc54352697206b2275337
DenkingOfficial <shershnev.andrei@gmail.com>
2023-01-19 00:47:16 +0500

    Forgot the actual script LOL

---
+++ scripts/crop_image.py
@@ -0,0 +1,23 @@
+import smartcrop
+from PIL import Image
+
+
+def crop_image(image, width=128, height=128):
+    new_image = Image.new('RGB', image.size)
+    new_image.paste(image)
+    image = new_image
+
+    sc = smartcrop.SmartCrop()
+
+    result = sc.crop(image, width=100, height=int(width / height * 100))
+
+    box = (
+        result['top_crop']['x'],
+        result['top_crop']['y'],
+        result['top_crop']['width'] + result['top_crop']['x'],
+        result['top_crop']['height'] + result['top_crop']['y']
+    )
+
+    cropped_image = image.crop(box)
+    cropped_image.thumbnail((width, height), Image.ANTIALIAS)
+    return cropped_image
